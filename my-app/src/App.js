e8e871d957d212b06fb49b5f926b8f19ca4702f9
Tyler <nuvallo.inc@gmail.com>
2019-12-12 21:41:57 -0500

    All dependencies installed and file structure set up

---
+++ my-app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import OnBoard from "./components/Form";
+import "./App.css";
+
+function App() {
+  return <OnBoard />;
+}
+
+export default App;
---
+++ my-app/src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+function OnBoard() {
+  return <h1>hello</h1>;
+}
+
+export default OnBoard;
