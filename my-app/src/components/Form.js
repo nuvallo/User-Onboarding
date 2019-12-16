1b3988c74e6d04fd7fe2c19fff82c5a5c32eb455
Tyler <nuvallo.inc@gmail.com>
2019-12-16 14:11:59 -0500

    Form created with Formik

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,19 +1,42 @@
-import React from "react";
-import { Formik, Form, Field } from "formik";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-function OnBoard() {
+function UserForm({ values, errors, touched, status, users, setUsers }) {
   return (
-    <Formik>
+    <div className="user-form">
       <Form>
-        <Field placeHolder="Enter Name" />
-        <Field placeHolder="Enter Email" />
-        <Field type="password" placeHolder="Enter password" />
+        <label htmlFor="name">
+          Name
+          <Field type="text" name="name" placeholder="Name" />
+        </label>
+        <label htmlFor="email">
+          Email
+          <Field type="email" name="email" placeholder="Email" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <Field type="password" name="password" placeholder="Password" />
+        </label>
+        <label htmlFor="terms">
+          <Field type="checkbox" name="terms" required />
+          Accept Terms of Service
+        </label>
         <button type="submit">Submit</button>
       </Form>
-    </Formik>
+    </div>
   );
 }
 
-export default OnBoard;
+const FormikUserForm = withFormik({
+  mapPropsToValues(props) {
+    return {
+      name: props.name || "",
+      email: props.email || "",
+      password: props.password || "",
+      terms: props.terms || false
+    };
+  }
+})(UserForm);
+export default FormikUserForm;
